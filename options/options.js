ada97bf6770810e3968e74a42c2427ab1c50695a
Crowdbotics <team@crowdbotics.com>
2024-01-17 19:54:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cricket-guru-45408",
+  url: "https://cricket-guru-45408.botics.co",
+  api: "https://cricket-guru-45408.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
